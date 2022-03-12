001c5df521d94cf76fcb46a8bee53a02f55b0b1b
neisantos <tecnicoeletronei@gmail.com>
2022-03-12 10:49:20 -0400

    inclusão de sexo

--- src/java.java
+++ src/java.java
@@ -6,11 +6,14 @@ public class java {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		String sexo = "Masculino";
 		String nome = "Francisnei";
 		int idade = 51;
-		double renda = 50000;
+		double renda = 150000;
+		
 	
-		System.out.printf(" %s\n tem %d anos\n e ganha R$ %.2f reais %n", nome, idade, renda);
+		System.out.printf(" %s\n tem : %d anos\n ganha: R$ %.2f reais %n %s sexo:\n", nome, idade, renda, sexo);
 		
 		Locale.setDefault(Locale.US);
 	
