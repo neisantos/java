e8ef18e816185f93f1e37da2a8befd22bd67e2b5
neisantos <tecnicoeletronei@gmail.com>
2022-03-12 10:33:05 -0400

    renda

---
+++ src/java.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class java {
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String nome = "Francisnei";
+		int idade = 51;
+		double renda = 2000;
+	
+		System.out.printf(" %s\n tem %d anos\n e ganha R$ %.2f reais %n", nome, idade, renda);
+		
+		Locale.setDefault(Locale.US);
+	
+		
+		sc.close();
+	}
+
+}
