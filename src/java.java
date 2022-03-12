a4c1ea9e74d5a59fda4db738ddc938920bb8f0d3
neisantos <tecnicoeletronei@gmail.com>
2022-03-12 10:57:22 -0400

    inclusão da experiência

--- src/java.java
+++ src/java.java
@@ -7,13 +7,15 @@ public class java {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		
 		String sexo = "Masculino";
 		String nome = "Francisnei";
 		int idade = 51;
 		double renda = 150000;
+		String conhecimento = "Android Studio, JAVA, LINUX, C, C#";
 		
 	
-		System.out.printf(" %s\n tem : %d anos\n ganha: R$ %.2f reais %n %s sexo:\n", nome, idade, renda, sexo);
+		System.out.printf(" %s\n tem : %d anos\n ganha: R$ %.2f reais %n %s sexo:\n  Experiência %s\n", nome, idade, renda, sexo, conhecimento);
 		
 		Locale.setDefault(Locale.US);
 	
